f9fec79809a714dc3b20a021474d0539b85ec431
KapitaZZZ <agarussky@gmail.com>
2018-12-24 20:55:38 +0500

    надо доделать сравнение карт

--- cards.py
+++ cards.py
@@ -13,26 +13,51 @@ print('Привет,', player2)
 card_suit = randint(1, 4)
 card_mean = randint(6, 14)
 
-# Присовение масти к номеру
+def card_player(mean, suit):
+    # Присовение масти к номеру
 
-if card_suit == 1:
-    suit = 'Черви'
-elif card_suit == 2:
-    suit = 'Буби'
-elif card_suit == 3:
-    suit = 'Крести'
-elif card_suit == 4:
-    suit = 'Пики'
+    if card_suit == 1:
+        suit = 'Черви'
+    elif card_suit == 2:
+        suit = 'Буби'
+    elif card_suit == 3:
+        suit = 'Крести'
+    elif card_suit == 4:
+        suit = 'Пики'
+
+    # Присвоение достоинства карты к номеру
+
+    if card_mean == 6:
+        mean = 'Шестерка'
+    elif card_mean == 7:
+        mean = 'Семерка'
+    elif card_mean == 8:
+        mean = 'Восьмерка'
+    elif card_mean == 9:
+        mean = 'Девятка'
+    elif card_mean == 10:
+        mean = 'Десятка'
+    elif card_mean == 11:
+        mean = 'Валет'
+    elif card_mean == 12:
+        mean = 'Дама'
+    elif card_mean == 13:
+        mean = 'Король'
+    elif card_mean == 14:
+        mean = 'Туз'
+
+    return (mean, suit)
+
+player_one_card = card_player(mean=card_mean, suit=card_suit)
+
+card_suit = randint(1, 4)
+card_mean = randint(6, 14)
+
+player_two_card = card_player(mean=card_mean, suit=card_suit)
 
 
-# Присвоение достоинства карты к номеру
+print(player1, 'выпала карта', player_one_card)
+print(player2, 'выпала карта',player_two_card)
 
-if card_mean == 6:
-    mean = 'Шестерка'
-elif card_mean == 7:
-    mean = 'Семерка'
-elif card_mean == 8:
-    mean = 'Восьмерка'
-elif card_mean == 9:
-    mean = 'Девятка'
-    elif
+# сравнение карт
+
